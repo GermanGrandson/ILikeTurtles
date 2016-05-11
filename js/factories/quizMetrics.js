8a3163e9093cdca8a347bd14fce62d8f10e27eec
German Grandson <ganieto1@gmail.com>
2016-05-11 15:24:01 -0400

    create Quiz factory

---
+++ js/factories/quizMetrics.js
@@ -0,0 +1,11 @@
+(fucntion(){
+  angular.module
+         .factory("quizMetrics", QuizMetrics);
+
+         function QuizMetrics(){
+           var quizObj = {
+             quizActive: false
+           };
+
+         }
+})();
