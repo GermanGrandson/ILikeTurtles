d7f93519f5429c68f97a865b1d8d1381268f2b03
German Grandson <ganieto1@gmail.com>
2016-05-11 23:50:52 -0400

    DataService Factory

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -3,11 +3,13 @@
   angular.module("turtleFacts")
          .controller("quizCtrl", QuizController);
 
-         QuizController.$inject = {'quizMetrics'}
+         QuizController.$inject = {'quizMetrics', 'DataService'}
 
-         function QuizController(quizMetrics){
+         function QuizController(quizMetrics, DataService){
 
            var vm = this;
            vm.quizMetrics = quizMetrics;
-         };
+         }
+
+
 })();
