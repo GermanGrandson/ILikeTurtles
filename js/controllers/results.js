201e7b0ff61659f3204fc5907fe815ac41cd1d55
German Grandson <ganieto1@gmail.com>
2016-05-13 16:09:46 -0400

    results controller logic with ng-show ng-repeat for wrongright answers

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -9,5 +9,17 @@ ResultsController.$inject = ['quizMetrics', 'DataService'];
            var vm = this;
            vm.quizMetrics = quizMetrics;
            vm.dataService = DataService;
+           vm.activeQuestion = 0;
+           vm.getAnswerClass = getAnswerClass;
+
+           function getAnswerClass(index){
+              if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+                return 'bg-success';
+              }else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
+                return 'bg-danger';
+              }
+           }
+
          }
+
 })();
